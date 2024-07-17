d42f5b2b74b61824d69896ef546bfa4d7a545533
zahidhasannipu01 <zahidhasannipu01@gmail.com>
2024-07-18 00:46:09 +0600

    Dashboard Blank Create React App Template

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Sidebar from "../components/Sidebar/Sidebar";
 import { Outlet } from "react-router";
+import SidebarMobile from "../components/Sidebar/mobile/SidebarMobile";
 
 const MainLayout = () => {
 	return (
 		<div className='flex'>
-			<Sidebar />
+			<div className='hidden md:block xl:block 2xl:block'>
+				<Sidebar />
+			</div>
+			<div className='block md:hidden xl:hidden 2xl:hidden'>
+				{" "}
+				<SidebarMobile />
+			</div>
+
 			<div className=''>
 				<Outlet />
 			</div>
